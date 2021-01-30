50fdde8b9619aedb038c731666c29e70bc70f606
Abilio Azevedo <abilio10@gmail.com>
2021-01-30 19:11:13 -0300

    feat: typescript

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
-import Head from 'next/head';
-import { motion } from 'framer-motion';
-import { useRouter } from 'next/router';
+import React from "react";
+import styled from "styled-components";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import Link from '../src/components/Link';
-import QuizLogo from '../src/components/QuizLogo';
-import QuizBackground from '../src/components/QuizBackground';
-import Footer from '../src/components/Footer';
-import GitHubCorner from '../src/components/GitHubCorner';
-import Input from '../src/components/Input';
-import Button from '../src/components/Button';
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import Link from "../src/components/Link";
+import QuizLogo from "../src/components/QuizLogo";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+import Input from "../src/components/Input";
+import Button from "../src/components/Button";
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -27,15 +34,12 @@ const QuizContainer = styled.div`
 
 export default function Home() {
   const router = useRouter();
-  const [name, setName] = React.useState('');
+  const [name, setName] = React.useState("");
 
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>
-          AluraQuiz -
-          {db.title}
-        </title>
+        <title>AluraQuiz -{db.title}</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
@@ -43,8 +47,8 @@ export default function Home() {
           as={motion.section}
           transition={{ delay: 0, duration: 0.5 }}
           variants={{
-            show: { opacity: 1, y: '0' },
-            hidden: { opacity: 0, y: '100%' },
+            show: { opacity: 1, y: "0" },
+            hidden: { opacity: 0, y: "100%" },
           }}
           initial="hidden"
           animate="show"
@@ -58,12 +62,14 @@ export default function Home() {
               onSubmit={function (infosDoEvento) {
                 infosDoEvento.preventDefault();
                 router.push(`/quiz?name=${name}`);
-                console.log('Fazendo uma submissão por meio do react');
+                console.log("Fazendo uma submissão por meio do react");
               }}
             >
               <Input
                 name="nomeDoUsuario"
-                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                onChange={(infosDoEvento) =>
+                  setName(infosDoEvento.target.value)
+                }
                 placeholder="Diz ai seu nome"
                 value={name}
               />
@@ -90,10 +96,10 @@ export default function Home() {
             <ul>
               {db.external.map((linkExterno) => {
                 const [projectName, githubUser] = linkExterno
-                  .replace(/\//g, '')
-                  .replace('https:', '')
-                  .replace('.vercel.app', '')
-                  .split('.');
+                  .replace(/\//g, "")
+                  .replace("https:", "")
+                  .replace(".vercel.app", "")
+                  .split(".");
 
                 return (
                   <li key={linkExterno}>
@@ -120,7 +126,7 @@ export default function Home() {
           animate="show"
         />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/kibolho/aluraquiz-abilioazevedo" />
+      <GitHubCorner projectUrl="https://github.com/kibolho/aluraquiz-base" />
     </QuizBackground>
   );
 }
