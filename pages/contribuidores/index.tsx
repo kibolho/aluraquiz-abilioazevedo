f83c69676da9f83a3c108114dd24c402ed4aaa72
Abilio Azevedo <abilio10@gmail.com>
2021-01-31 22:16:06 -0300

    fix: theme provider

--- pages/contribuidores/index.tsx
+++ pages/contribuidores/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import fs from 'fs';
-import NextImage from 'next/image';
-import styled from 'styled-components';
-import Widget from '../../src/components/Widget';
-import QuizLogo from '../../src/components/QuizLogo';
-import QuizBackground from '../../src/components/QuizBackground';
-import Footer from '../../src/components/Footer';
-import GitHubCorner from '../../src/components/GitHubCorner';
+import React from "react";
+import fs from "fs";
+import NextImage from "next/image";
+import styled from "styled-components";
+import Widget from "../../src/components/Widget";
+import QuizLogo from "../../src/components/QuizLogo";
+import QuizBackground from "../../src/components/QuizBackground";
+import Footer from "../../src/components/Footer";
+import GitHubCorner from "../../src/components/GitHubCorner";
+import { fetchQuizes, filterMyQuiz } from "src/utils/apiContentful";
+import { addApolloState, initializeApollo } from "@/lib/apollo/client";
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -20,89 +22,91 @@ export const QuizContainer = styled.div`
 `;
 
 function Image({ src }) {
-  const baseUrl = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : 'https://abilioazevedo.com.br';
+  const baseUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://abilioazevedo.com.br";
 
   const thumbnail = `${baseUrl}/api/screenshot.png?url=${encodeURIComponent(
-    src,
+    src
   )}`;
   return (
-    <a href={src} style={{ display: 'inline-block', fontSize: '0' }}>
-      <NextImage
-        width="1024"
-        height="768"
-        src={thumbnail}
-      />
+    <a href={src} style={{ display: "inline-block", fontSize: "0" }}>
+      <NextImage width="1024" height="768" src={thumbnail} />
     </a>
   );
 }
 
-export default function ContributorsPage({ contributors }) {
+export default function ContributorsPage({ contributors, quiz }) {
   return (
-    <QuizBackground backgroundImage="https://www.alura.com.br/assets/img/imersoes/react-2/fundo-do-mar-imersao-react-2-01.1609262503.svg">
-      <QuizContainer
-        style={{ margin: 'auto', padding: '5%', maxWidth: '1400px' }}
-      >
-        <QuizLogo />
-        <Widget
-          style={{ maxWidth: '350px', marginLeft: 'auto', marginRight: 'auto' }}
+    <ThemeProvider theme={quiz.theme}>
+      <QuizBackground backgroundImage="https://www.alura.com.br/assets/img/imersoes/react-2/fundo-do-mar-imersao-react-2-01.1609262503.svg">
+        <QuizContainer
+          style={{ margin: "auto", padding: "5%", maxWidth: "1400px" }}
         >
-          <Widget.Header style={{ justifyContent: 'center' }}>
-            <h1 style={{ fontSize: '25px' }}>Galeria de Projetos</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>
-              Estamos muito felizes de contar com a sua participação, confira
-              todos os outros projetos criados durante essa imersão!
-            </p>
-          </Widget.Content>
-        </Widget>
+          <QuizLogo />
+          <Widget
+            style={{
+              maxWidth: "350px",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Widget.Header style={{ justifyContent: "center" }}>
+              <h1 style={{ fontSize: "25px" }}>Galeria de Projetos</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>
+                Estamos muito felizes de contar com a sua participação, confira
+                todos os outros projetos criados durante essa imersão!
+              </p>
+            </Widget.Content>
+          </Widget>
 
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-            gridGap: '1em',
-          }}
-        >
-          {contributors.map(({ user, projectUrl }, indice) => (
-            <Widget key={indice} style={{ maxWidth: '400px' }}>
-              <Widget.Header style={{ alignItems: 'center' }}>
-                <img
-                  width="25"
-                  height="25"
-                  src={`https://github.com/${user}.png`}
-                  style={{ marginRight: '15px', borderRadius: '100%' }}
-                />
-                <h2>
-                  <a
-                    href={`https://github.com/${user}`}
-                    style={{ color: 'inherit' }}
-                  >
-                    @
-                    {user}
-                  </a>
-                </h2>
-              </Widget.Header>
-              <Widget.Content style={{ padding: 0 }}>
-                <Image src={projectUrl} />
-              </Widget.Content>
-            </Widget>
-          ))}
-        </div>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/kibolho" />
-    </QuizBackground>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+              gridGap: "1em",
+            }}
+          >
+            {contributors.map(({ user, projectUrl }, indice) => (
+              <Widget key={indice} style={{ maxWidth: "400px" }}>
+                <Widget.Header style={{ alignItems: "center" }}>
+                  <img
+                    width="25"
+                    height="25"
+                    src={`https://github.com/${user}.png`}
+                    style={{ marginRight: "15px", borderRadius: "100%" }}
+                  />
+                  <h2>
+                    <a
+                      href={`https://github.com/${user}`}
+                      style={{ color: "inherit" }}
+                    >
+                      @{user}
+                    </a>
+                  </h2>
+                </Widget.Header>
+                <Widget.Content style={{ padding: 0 }}>
+                  <Image src={projectUrl} />
+                </Widget.Content>
+              </Widget>
+            ))}
+          </div>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/kibolho" />
+      </QuizBackground>
+    </ThemeProvider>
   );
 }
 
 export async function getStaticProps() {
-  const contributors = fs.readdirSync('./contributors').map((fileName) => {
+  const contributors = fs.readdirSync("./contributors").map((fileName) => {
     const [user, projectUrl] = fs
-      .readFileSync(`./contributors/${fileName}`, { encoding: 'utf-8' })
-      .split('\n');
+      .readFileSync(`./contributors/${fileName}`, { encoding: "utf-8" })
+      .split("\n");
 
     return {
       user,
@@ -110,9 +114,16 @@ export async function getStaticProps() {
     };
   });
 
-  return {
+  const quizes = await fetchQuizes();
+  const myQuiz = filterMyQuiz(quizes);
+
+  const apolloClient = initializeApollo();
+
+  return addApolloState(apolloClient, {
     props: {
       contributors,
+      quiz: myQuiz?.rawQuiz,
     },
-  };
+    revalidate: 1,
+  });
 }
